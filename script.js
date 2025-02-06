c4e769fa08cbcfba5bc4142df3a880d8f9c5ec88
Geovani de Oliveira Fanine <50090171+geovaniof@users.noreply.github.com>
2025-02-06 00:50:39 -0300

    Aula 06 - Comandos de Entrada

---
+++ script.js
@@ -0,0 +1,9 @@
+var nome = prompt("Qual o seu nome?")
+
+var sobreNome = prompt("Qual seu sobrenome ?")
+
+console.log('Olá ' + nome + ' ' + sobreNome);
+
+document.write('<h1>Bem vindo ' + nome +'</h1>');
+
+document.write('<img src="https:/sujeitoprogramador.com/steve.png" alt="Foto do Steve"/>');
